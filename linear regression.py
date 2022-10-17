951c1234def5c79f98860f94047aeafe7f31f1b9
sangji <lsgsh211@gmail.com>
2022-10-17 14:38:20 +0900

    add .

--- linear regression.py
+++ linear regression.py
@@ -1,9 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = np.loadtxt('linear regression\data.csv', delimiter=',')
+data = np.loadtxt('data.csv', delimiter=',')
 x = data[:, 0]
 y = data[:, 1]
+x = x[0:int(len(x)*0.7)]
+y = y[0:int(len(y)*0.7)]
+test_data_x = x[int(len(x)*0.7):]
+test_data_y = y[int(len(y)*0.7):]
 
 w = 0
 b = 0
@@ -26,3 +30,14 @@ y_pred = w * x + b
 plt.scatter(x, y)
 plt.plot([min(x), max(x)], [min(y_pred), max(y_pred)], color='red')
 plt.show()
+
+ys = 0
+for i in range(len(test_data_x)):
+    ys += abs(test_data_y[i] - (w * test_data_x[i] + b)) / test_data_y[i]
+
+print((1-ys / len(test_data_x)) * 100)
+"""
+plt.scatter(test_data_x, test_data_y)
+plt.plot([min(test_data_x), max(test_data_x)], [min(y_pred), max(y_pred)], color='red')
+plt.show()
+"""
